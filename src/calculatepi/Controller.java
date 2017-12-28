30796e9ac0e143922b81d768442f8c2ed41864de
Daniel Russom <daniel.russom00@gmail.com>
2017-12-28 21:23:23 +0000

    Timer
    - Added timer to track calculation time

--- src/calculatepi/Controller.java
+++ src/calculatepi/Controller.java
@@ -3,18 +3,22 @@ package calculatepi;
 public class Controller {
 
 	public static void main(String[] args) {
-		//TODO Replace with BigDecimal?
+		// TODO Replace with BigDecimal?
 		double pi = 1;
 		boolean flag = false;
-		for(double i = 3; i < 1000000000; i+=2) {
-			if(flag) {
-				pi+=(1/i);
+		long startTime = System.currentTimeMillis();
+		for (double i = 3; i < 1000000000; i += 2) {
+			if (flag) {
+				pi += (1 / i);
 				flag = false;
 			} else {
-				pi-=(1/i);
+				pi -= (1 / i);
 				flag = true;
 			}
 		}
-		System.out.println(pi*4);
+		long endTime = System.currentTimeMillis();
+		long duration = (endTime - startTime);
+		System.out.println(pi * 4);
+		System.out.println("Calculated in " + duration + " milliseconds.");
 	}
 }
