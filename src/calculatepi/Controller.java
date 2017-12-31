a1b45caa351e43c8a63ca8e2219c28073c05258a
Daniel Russom <daniel.russom00@gmail.com>
2017-12-31 21:42:58 +0000

    Generate Pi to 31 places
    - Changed method to generate Pi
    - Now generates pi to 30 places
    - Changed timer to show time in nanoseconds.

--- src/calculatepi/Controller.java
+++ src/calculatepi/Controller.java
@@ -3,22 +3,31 @@ package calculatepi;
 public class Controller {
 
 	public static void main(String[] args) {
-		// TODO Replace with BigDecimal?
-		double pi = 1;
-		boolean flag = false;
-		long startTime = System.currentTimeMillis();
-		for (double i = 3; i < 1000000000; i += 2) {
-			if (flag) {
-				pi += (1 / i);
-				flag = false;
-			} else {
-				pi -= (1 / i);
-				flag = true;
-			}
+		int digits = 100;
+		String pi = "";
+		int[] arr = new int[digits + 1];
+		int carry = 0;
+		long startTime = System.nanoTime();
+		// Initializes the array with a value of 2000
+		for (int i = 0; i <= digits; ++i) {
+			arr[i] = 2000;
 		}
-		long endTime = System.currentTimeMillis();
+
+		for (int i = digits; i > 0; i -= 14) {
+			int sum = 0;
+			for (int j = i; j > 0; --j) {
+				sum = sum * j + 10000 * arr[j];
+				arr[j] = sum % (j * 2 - 1);
+				sum = sum / (j * 2 - 1);
+			}
+			pi = pi + (carry + sum / 10000);
+			carry = sum % 10000;
+		}
+		long endTime = System.nanoTime();
 		long duration = (endTime - startTime);
-		System.out.println(pi * 4);
-		System.out.println("Calculated in " + duration + " milliseconds.");
+		System.out.println("Calculated in " + duration + " nanoseconds.");
+		// Puts a decimal point into the generated string
+		pi = pi.substring(0, 1) + "." + pi.substring(1);
+		System.out.println(pi);
 	}
 }
